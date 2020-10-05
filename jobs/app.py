ae587b2caee30ea3ae38995af5d339d640b8748c
Amit Anand <anand.amit943@gmail.com>
2020-10-05 14:30:31 -0700

    fix for exceute_sql function

--- jobs/app.py
+++ jobs/app.py
@@ -13,15 +13,15 @@ def open_connection():
     row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, commit = False, single = False, values = ()):
+def execute_sql(sql,values = (), commit = False, single = False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit is True:
+    if commit == True:
         results = connection.commit()
     else:
-        cursor.fetchone() if single else cursor.fetchall()
-        cursor.close()
+        results = cursor.fetchone() if single else cursor.fetchall()
 
+    cursor.close()
     return results
 
 @app.teardown_appcontext
