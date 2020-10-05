1e528f10ff2aa7901a2157f9f0560d0ea3311262
Amit Anand <anand.amit943@gmail.com>
2020-10-05 14:37:42 -0700

    fix in open_connection

--- jobs/app.py
+++ jobs/app.py
@@ -5,12 +5,11 @@ PATH = "db/jobs.sqlite"
 app = Flask(__name__)
 
 def open_connection():
-    attr = getattr(g, '_connection', None)
-    connection = attr
-    if connection is None:
-        connection = sqlite3.connect(PATH)
-        g._connection = PATH
-    row_factory = sqlite3.Row
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql,values=(), commit=False, single = False):
@@ -20,7 +19,7 @@ def execute_sql(sql,values=(), commit=False, single = False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-        
+
     cursor.close()
     return results
 
